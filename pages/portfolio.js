ade0a928675e7af643a4ad133d65e4872f2fc3c9
ArashZich <zenith.arash@gmail.com>
2020-09-03 15:20:32 +0430

    test server get data

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,14 +1,31 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 import { withRouter } from "next/router";
+import axios from "axios";
 
 function Portfolio(props) {
+  const { portfolio } = props;
   return (
     <BaseLayout>
-      <h1>I am Portfolio page</h1>
-      <h2>{props.router.query.title}</h2>
+      <h1> {portfolio.title} </h1>
+      <p> Body: {portfolio.body} </p>
+      <p> ID: {portfolio.id} </p>
     </BaseLayout>
   );
 }
+
+Portfolio.getInitialProps = async ({ query }) => {
+  const portfolioId = query.id;
+  let portfolio = {};
+  try {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${portfolioId}`
+    );
+    portfolio = response.data;
+  } catch (err) {
+    console.error(err);
+  }
+  return { portfolio };
+};
 
 export default withRouter(Portfolio);
